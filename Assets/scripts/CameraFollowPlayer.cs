f70f8e8cf0b09e71b93fa83b283ddebfe048ad26
minichibis <minichibis@gmail.com>
2020-09-02 16:07:10 -0500

    do you like, my car (my car)

---
+++ Assets/scripts/CameraFollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿/** Sam Carpenter
+* Assignment 1
+* Moves the camera to follow the player
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	public GameObject player;
+	
+	public Vector3 offset = new Vector3(0, 5, -10);
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/scripts/PlayerControls.cs
@@ -0,0 +1,56 @@
+﻿/** Sam Carpenter
+* Assignment 1
+* make the player move about
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+	public float speed;
+	public float maxspeed;
+	public float turnspeed;
+	public float maxturnspeed;
+	public float hInput;
+	public float vInput;
+	public bool prevspeed;
+	public bool prevturn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		hInput = Input.GetAxis("Horizontal");
+		vInput = Input.GetAxis("Vertical");
+		if(prevspeed){
+			speed = Mathf.Lerp(speed, maxspeed, 0.05f);
+		}else{
+			speed = Mathf.Lerp(speed, 0, 0.05f);
+		}
+		if(prevturn){
+			turnspeed = Mathf.Lerp(turnspeed, maxturnspeed * Mathf.Sign(hInput), 0.05f);
+		}else{
+			turnspeed = Mathf.Lerp(turnspeed, 0, 0.05f);
+		}
+		
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+		transform.Rotate(Vector3.up * turnspeed * Time.deltaTime);
+		
+		prevspeed = false;
+		if(vInput > 0){
+			prevspeed = true;
+		}
+		prevturn = false;
+		if(hInput != 0){
+			prevturn = true;
+		}
+		
+    }
+}
